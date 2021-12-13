f74c94f4cf7aa9bbc89f32a0ac6288161dc74c75
Langenfeld <langenfv@informatik.uni-freiburg.de>
2021-12-13 10:36:24 +0100

    increased minor version because of api changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='py-gitea',
-    version='0.1.10',
+    version='0.2.0',
     description='A python wrapper for the Gitea API',
     long_description_content_type="text/markdown",
     long_description=README,
