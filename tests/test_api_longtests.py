6ec1831bc2eb63bc0632499b95823257f73825a3
Langenfeld <langenfv@informatik.uni-freiburg.de>
2026-06-26 14:28:50 +0200

    test: fix tests for branches and repo tags

--- tests/test_api_longtests.py
+++ tests/test_api_longtests.py
@@ -1,101 +1,79 @@
-import pytest
-import uuid
+from unittest import TestCase
+from utils import suid
 
-from gitea import Gitea, User, Organization, Team, Repository, Issue
-from gitea import NotFoundException, AlreadyExistsException
-
-
-# put a ".token" file into your directory containg only the token for gitea
-@pytest.fixture
-def instance(scope="module"):
-    try:
-        g = Gitea("http://localhost:3000", open(".token", "r").read().strip())
-        print("Gitea Version: " + g.get_version())
-        print("API-Token belongs to user: " + g.get_user().username)
-        return g
-    except:
-        assert (
-            False
-        ), "Gitea could not load. \
-                - Instance running at http://localhost:3000 \
-                - Token at .token   \
-                    ?"
+from gitea import (
+    Gitea,
+    Issue,
+)
 
 
-# make up some fresh names for the tests run
-test_org = "org_" + uuid.uuid4().hex[:8]
-test_user = "user_" + uuid.uuid4().hex[:8]
-test_team = (
-    "team_" + uuid.uuid4().hex[:8]
-)  # team names seem to have a rather low max lenght
-test_repo = "repo_" + uuid.uuid4().hex[:8]
+class PublicApiTest(TestCase):
+    def setUp(self):
+        """Set up a user, repo and TODO TOD"""
+        self.test_org_name = "org_public_" + suid()
+        self.test_user_name = "user_" + suid()
+        self.test_team_name = "team_" + suid()  # team names seem to have a rather low max lenght
+        self.test_repo_name = "repo_" + suid()
+        # get admin gitea instance to set up tests
+        try:
+            g = Gitea("http://localhost:3000", open(".token", "r").read().strip())
+            print("Gitea Version: " + g.get_version())
+            print("API-Token belongs to user: " + g.get_user().username)
+        except:
+            assert (
+                False
+            ), "Gitea could not load. \
+                    - Instance running at http://localhost:3000 \
+                    - Token at .token   \
+                        ?"
+        self.g = g
+        self.user = g.get_user()
+        self.org = g.create_org(self.user, self.test_org_name, "some-desc", "loc")
+        self.repo = g.create_repo(self.org, self.test_repo_name, "user owned repo")
 
 
-def prepare_test_org(instance):
-    try:
-        return Organization.request(instance, test_org)
-    except NotFoundException:
-        user = instance.create_user(
-            test_user, test_user + "@example.org", "abcdefg1.23AB", send_notify=False
-        )
-        return instance.create_org(user, test_org, "some Description for longtests")
+    def test_list_repos(self):
+        repos = self.org.get_repositories()
+        assert len(repos) == 1
+        # test a number of repository listings larger than the pagination number (default 50)
+        for i in range(1, 54):
+            self.g.create_repo(self.org, self.test_repo_name + "_" + str(i), str(i))
+        repos = self.org.get_repositories()
+        assert len(repos) >= 53
 
 
-def test_list_repos(instance):
-    org = prepare_test_org(instance)
-    repos = org.get_repositories()
-    assert len(repos) == 0
-    # test a number of repository listings larger than the pagination number (default 50)
-    for i in range(1, 54):
-        instance.create_repo(org, test_repo + "_" + str(i), str(i))
-    repos = org.get_repositories()
-    assert len(repos) >= 53
+    def test_list_issue(self):
+        for x in range(0, 100):
+            Issue.create_issue(
+                self.g,
+                self.repo,
+                "TestIssue" + str(x),
+                "We will be too many to be listed on one page",
+            )
+        issues = self.repo.get_issues()
+        assert len(issues) > 98
 
 
-def test_list_issue(instance):
-    org = prepare_test_org(instance)
-    repo = instance.create_repo(
-        org, test_repo, "Testing a huge number of Issues and how they are listed"
-    )
-    for x in range(0, 100):
-        Issue.create_issue(
-            instance,
-            repo,
-            "TestIssue" + str(x),
-            "We will be too many to be listed on one page",
-        )
-    issues = repo.get_issues()
-    assert len(issues) > 98
+    def test_list_branches(self):
+        branches = self.repo.get_branches()
+        master = [b for b in branches if b.name == "master"]
+        assert len(master) > 0
 
+        for x in range(0, 54):
+            self.repo.add_branch(master[0], "test_branch_" + str(x) + "_" + suid())
 
-def test_list_branches(instance):
-    org = prepare_test_org(instance)
-    repo = instance.create_repo(
-        org, test_repo, "Testing a huge number of Branches and how they are listed"
-    )
-    branches = repo.get_branches()
-    master = [b for b in branches if b.name == "master"]
-    assert len(master) > 0
+        branches = self.repo.get_branches()
+        assert len(branches) >= 53
 
-    for x in range(0, 54):
-        repo.add_branch(master[0], "test_branch_" + str(x) + "_" + str(uuid.uuid4().hex[:8]))
+    def test_list_tags(self):
+        branches = self.repo.get_branches()
+        master = [b for b in branches if b.name == "master"]
+        assert len(master) > 0
 
-    branches = repo.get_branches()
-    assert len(branches) >= 53
+        # Add all tags on last 'master' branch commit
+        for x in range(0, 54):
+            self.repo.add_tag("test_tag_" + str(x) + "_" + suid(), master[0].commit["id"], "test tag")
 
-def test_list_tags(instance):
-    org = prepare_test_org(instance)
-    repo = instance.create_repo(
-        org, test_repo, "Testing a huge number of Tags and how they are listed"
-    )
-    branches = repo.get_branches()
-    master = [b for b in branches if b.name == "master"]
-    assert len(master) > 0
+        tags = self.repo.get_tags()
+        assert len(tags) >= 53
 
-    # Add all tags on last 'master' branch commit
-    for x in range(0, 54):
-        repo.add_tag("test_tag_" + str(x)  + "_" + str(uuid.uuid4().hex[:8]), master[0].commit["id"], "test tag")
-
-    tags = repo.get_tags()
-    assert len(tags) >= 53
-    
--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,9 @@
+import uuid
+
+
 def generate_random_color():
     import random
     return "{:06x}".format(random.randint(0, 0xFFFFFF))
+
+def suid() -> str:
+    return str(uuid.uuid4().hex[:8])
